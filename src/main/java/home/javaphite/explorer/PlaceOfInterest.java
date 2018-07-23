58398f3d9bc3383428a6699803947ac05b1691d5
Javaphite <javaphite@gmail.com>
2018-07-23 23:07:44 +0300

    Add PlaceOfInterest wrapper for Item.
    Wrapper makes text fields if Items reusable in mutable objects
    representing unique items in unique location (kind of Flyweight pattern).

---
+++ src/main/java/home/javaphite/explorer/PlaceOfInterest.java
@@ -0,0 +1,23 @@
+package home.javaphite.explorer;
+
+public class PlaceOfInterest {
+    private final int id = (int) Math.abs(super.hashCode()/111119);
+    private boolean scanned;
+    private Item associatedItem;
+
+    PlaceOfInterest(Item associatedItem) {
+        this.associatedItem = associatedItem;
+    }
+
+    public boolean isScanned() {
+        return scanned;
+    }
+
+    public Item getAssociatedItem() {
+        return associatedItem;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
