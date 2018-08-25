6604d2498960e544b440554741bab5bc3434d191
Javaphite <javaphite@gmail.com>
2018-08-25 19:51:01 +0300

    Location class refactored: 1. Randomized Location creation logic moved to separate method 2. Better names for methods and fields 3. Add logging

--- src/main/java/home/javaphite/explorer/model/Location.java
+++ src/main/java/home/javaphite/explorer/model/Location.java
@@ -2,6 +2,8 @@ package home.javaphite.explorer.model;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -13,67 +15,96 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Location {
-    public Map<Integer, PlaceOfInterest> pois = new HashMap<>();
+
+    private static final Logger LOG = LoggerFactory.getLogger(Location.class);
+
     private Relief relief;
 
-    enum Relief {
+    public Map<Integer, PlaceOfInterest> placesOfInterest;
+
+    public Location(Relief relief, Map<Integer, PlaceOfInterest> placesOfInterest) {
+        this.relief = relief;
+        this.placesOfInterest = new HashMap<>(placesOfInterest);
+    }
+
+    static Location randomized() {
+        Random random = new Random();
+
+        // Grab random relief
+        Relief[] reliefs = Relief.values();
+        int nReliefs = reliefs.length;
+        Relief relief = reliefs[random.nextInt(nReliefs)];
+
+        // Grab random place of interest for each placeholder in relief description
+        Map<Integer, PlaceOfInterest> pois = new HashMap<>();
+        Matcher matcher = Relief.PLACEHOLDER_PATTERN.matcher(relief.description);
+        while (matcher.find()) {
+            Item[] items = Item.values();
+            int nItems = items.length;
+            int randomItemOrdinal = random.nextInt(nItems);
+            Item randomItem = items[randomItemOrdinal];
+            PlaceOfInterest poi = new PlaceOfInterest(randomItem);
+
+            pois.put(poi.getId(), poi);
+        }
+
+        Location randomizedLocation = new Location(relief, pois);
+        LOG.debug("Created randomized Location - {}", randomizedLocation.relief);
+
+        return randomizedLocation;
+    }
+
+    public String details() {
+        StringBuilder details = new StringBuilder(140);
+
+        for (PlaceOfInterest poi: placesOfInterest.values()) {
+            details.append(poi)
+                    .append(": ")
+                    .append(poi.getStatus())
+                    .append(System.lineSeparator());
+        }
+
+        return details.toString();
+    }
+
+    @Override
+    public String toString() {
+        String result = relief.description;
+
+        for (PlaceOfInterest poi: placesOfInterest.values()) {
+            Matcher matcher = Relief.PLACEHOLDER_PATTERN.matcher(result);
+            result = matcher.replaceFirst(poi.toString());
+        }
+
+        return result;
+    }
+
+    private enum Relief {
+
         ROCKS("rocks");
 
         static final String DESCRIPTION_TEMPLATES_PATH = "/descriptions/relief_descriptions.json";
         static final Pattern PLACEHOLDER_PATTERN = Pattern.compile("[{]}");
 
-        private final String descriptionTemplate;
+        private final String description;
 
-        Relief(String descrTemplateId) {
+        Relief(String descriptionId) {
             ObjectMapper mapper = new ObjectMapper();
+
             try (InputStream inStream = getClass().getResourceAsStream(DESCRIPTION_TEMPLATES_PATH)) {
-                JsonNode node = mapper.readTree(inStream).get(descrTemplateId);
-                descriptionTemplate = node.get("description_template").asText();
-            } catch (IOException treeReadingError) {
+                JsonNode node = mapper.readTree(inStream).get(descriptionId);
+                description = node.get("description_template").asText();
+            }
+            catch (IOException treeReadingError) {
+                LOG.error("Relief descriptions JSON parsing error: ", treeReadingError);
                 throw new UncheckedIOException(treeReadingError);
             }
-        }
-    }
 
-    // Generates random location
-   public Location() {
-        // Grab random relief
-        Random randomGenerator = new Random();
-        int bound = Relief.values().length;
-        relief = Relief.values()[randomGenerator.nextInt(bound)];
-
-        // Grab random place of interest for each placeholder in relief description
-        Matcher matcher = Relief.PLACEHOLDER_PATTERN.matcher(relief.descriptionTemplate);
-        while (matcher.find()) {
-            int ordinalBound = Item.values().length;
-            int randomOrdinal = randomGenerator.nextInt(ordinalBound);
-            Item randomItem = Item.values()[randomOrdinal];
-            PlaceOfInterest poi = new PlaceOfInterest(randomItem);
-            pois.put(poi.getId(), poi);
+            LOG.debug("Relief variant successfully initialized: {}", descriptionId);
         }
-    }
-
-    @Override
-    public String toString() {
-        String result = relief.descriptionTemplate;
-        for (PlaceOfInterest poi: pois.values()) {
-            Matcher matcher = Relief.PLACEHOLDER_PATTERN.matcher(result);
-            result = matcher.replaceFirst(poi.toString());
-        }
-        return result;
-    }
-
-    public String getPoisStatuses() {
-        StringBuilder statuses = new StringBuilder(140);
-        for (PlaceOfInterest poi: pois.values()) {
-            statuses.append(poi)
-                    .append(": ")
-                    .append(poi.getStatus())
-                    .append(System.lineSeparator());
-        }
-        return statuses.toString();
     }
 }
+
 
 
 
