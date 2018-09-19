f2694c04ba67f315f2a2de0d64d92cd53cbf1fcf
Javaphite <javaphite@gmail.com>
2018-09-19 21:18:40 +0300

    New exception type for direction issues for move command.

---
+++ src/main/java/home/javaphite/explorer/model/exceptions/InvalidDirectionNameException.java
@@ -0,0 +1,8 @@
+package home.javaphite.explorer.model.exceptions;
+
+public class InvalidDirectionNameException extends IllegalArgumentException {
+
+    public InvalidDirectionNameException(String errorMessage, Throwable cause) {
+        super(errorMessage, cause);
+    }
+}
