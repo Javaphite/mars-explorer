32a9841f44a7ea1003920de3726b158dbec7d8a5
Javaphite <javaphite@gmail.com>
2018-09-25 21:37:44 +0300

    MarsExplorerApplication cleaning

--- src/main/java/home/javaphite/explorer/MarsExplorerApplication.java
+++ src/main/java/home/javaphite/explorer/MarsExplorerApplication.java
@@ -1,10 +1,13 @@
 package home.javaphite.explorer;
 
  import org.springframework.boot.SpringApplication;
+ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
  import org.springframework.boot.autoconfigure.SpringBootApplication;
+ import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
  import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 @ComponentScan(basePackages = "home.javaphite.explorer")
 public class MarsExplorerApplication {
 
